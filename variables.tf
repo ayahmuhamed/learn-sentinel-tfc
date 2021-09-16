0dcb84b8509112e3cde6074d7a2def1ea90e9672
ayahmuhamed <88331884+ayahmuhamed@users.noreply.github.com>
2021-09-17 01:53:07 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
 variable "region" {
   description = "AWS region"
   default = "us-west-1"
+  AWS_ACCESS_KEY_ID= "ASIAT4K63Z2RVLSULY6K"
+  AWS_SECRET_ACCESS_KEY= "5D6nfShUT/XrJp+rg6KTve1ES8Cmp72CSB2Ethlf"
 }
 
 variable "instance_type" {
